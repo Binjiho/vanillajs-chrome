07be3b4009d0736dadb4382da5c5592fc87b3182
Binjiho <jiyu1948@gmail.com>
2020-03-21 12:50:39 +0900

    Getting The Weather API

---
+++ weather.js
@@ -0,0 +1,57 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "0a94feea5abcd3efe1a05af0b3af6992";
+const COORDS = 'coords';
+
+function getWeather(lat,lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const place = json.name;
+        const description = json.weather[0].description;
+        const temperature = json.main.temp;
+        weather.innerText = `장소:${place}
+        온도: ${temperature}
+        날씨: ${description}`;
+    });
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS,JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj ={
+        latitude:latitude,
+        longitude:longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude);
+}
+
+
+function handleGeoError(){
+    console.log("can't access geo");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError)
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }else{
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude,parsedCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
